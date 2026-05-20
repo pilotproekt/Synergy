a54a429744c3c20581677f7201cb72193d36e0d4
pilotproekt <udovenkokol@gmail.com>
2026-05-21 00:11:43 +0500

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,9 @@
+print("Введите этапы развития человека по порядку:")
+stage1 = input("Первый этап: ") # Дриопитек
+stage2 = input("Второй этап: ") # австралопитек
+stage3 = input("Третий этап: ") # Человек умелый
+stage4 = input("Четвертый этап: ") # Человек прямоходящий
+stage5 = input("Пятый этап: ") # Неандерталец
+stage6 = input("Шестой этап: ") # Кроманьенец
+stage7 = input("Cедьмой этап: ") # Homo sapiens sapiens
+print(stage1, stage2, stage3, stage4, stage5, stage6, stage7, sep=" => ")
